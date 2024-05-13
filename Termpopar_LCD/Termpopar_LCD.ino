6fce06b228d668292a2ed9dd3dedaf0c4249ce84
Damian Eliab Carmona Lopez <damianeliabs@comunidad.unam.mx>
2024-05-13 11:08:34 -0600

    Se agrega al codigo la parte del Botón ON/OFF

--- Termpopar_LCD/Termpopar_LCD.ino
+++ Termpopar_LCD/Termpopar_LCD.ino
@@ -2,8 +2,11 @@
 //Compatible with the Arduino IDE 1.0
 //Library version:1.1
 #include <LiquidCrystal_I2C.h>
-
+#include <ezButton.h>
 #include "max6675.h"
+
+ezButton button(7);
+
 
 int thermoDO = 4;
 int thermoCS = 5;
@@ -20,14 +23,13 @@ uint8_t degree[8]  = {140,146,146,140,128,128,128,128};
 void setup()
 {
   Serial.begin(9600);
+  button.setDebounceTime(50); // set debounce time to 50 milliseconds
   lcd.init();                      // initialize the lcd 
   // Print a message to the LCD.
   lcd.backlight();
-  
   // wait for MAX chip to stabilize
   pinMode(vccPin, OUTPUT); digitalWrite(vccPin, HIGH);
   pinMode(gndPin, OUTPUT); digitalWrite(gndPin, LOW);
-
   lcd.begin(16, 2);
   lcd.createChar(0, degree);
   // wait for MAX chip to stabilize
@@ -37,24 +39,33 @@ void setup()
 
 void loop()
 {
-
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Temperatura:");
+   button.loop(); // MUST call the loop() function first
+   // MUST call the loop() function first){
+  if(button.getState()==0){
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Temperatura:");
+    // go to line #1
+    lcd.setCursor(0,1);
+    Serial.println("The button is pressed");
+    temperatura = thermocouple.readCelsius();
+    lcd.print(temperatura);
+    Serial.print(String(temperatura));
+    Serial.print("\n");
+  #if ARDUINO >= 100
+    lcd.write((byte)0);
+  #else
+    lcd.print(0, BYTE);
+  #endif
+    lcd.print("C ");
+    delay(1000);
+  }
+  if(button.getState()==1){
+     Serial.println("The button is released");
+     lcd.clear();
+     lcd.setCursor(0,0);
+     lcd.print("Sistema Apagado");
+     delay(300);
+  }
   
-  // go to line #1
-  lcd.setCursor(0,1);
-  temperatura = thermocouple.readCelsius();
-  lcd.print(temperatura);
-  Serial.print(String(temperatura));
-  Serial.print("\n");
-#if ARDUINO >= 100
-  lcd.write((byte)0);
-#else
-  lcd.print(0, BYTE);
-#endif
-  lcd.print("C ");
- 
-  delay(1000);
-
 }
